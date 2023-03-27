b9ad2236151f0e968960fe7b1c7b9cbf4dd00bdb
focxverr1 <117368249+focxverr1@users.noreply.github.com>
2023-03-27 18:54:22 +0400

    Add files via upload

---
+++ New folder (6)/main.js
@@ -0,0 +1,22 @@
+let web = `{
+    "name": "Lorem Ipsum",
+    "developer": "Davit Surameli",
+    "categorieas": ["Editorial", "lead", "BBC", "Sport", "Sustailnability"],
+    "time":"27 march 2023",
+    "comment":"27",
+    "img": "https://compote.slate.com/images/5294e6d0-53ed-4a4a-a350-7eaeab72ac93.jpeg?crop=1560%2C1040%2Cx0%2Cy0https://compote.slate.com/images/5294e6d0-53ed-4a4a-a350-7eaeab72ac93.jpeg?crop=1560%2C1040%2Cx0%2Cy0"
+}`;
+web = JSON.parse(web);
+console.log(web);
+
+// const objPerson = {
+//     "name": "aleko",
+//     "lname": "barliani",
+//     "age":"17",
+//     "hob":"exercises"
+// };
+// const person = JSON.parse(jsPerson);
+// console.log(jsPerson)
+// console.log(objPerson)
+
+
